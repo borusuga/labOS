185409e17a946f7f78ba3104bb59c2be51c58992
Alyona <alena210701@mail.ru>
2023-10-08 14:09:48 +0300

    Initial Commit

---
+++ labOS/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  labOS
+//
+//  Created by Alyona Borushnova on 08.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
