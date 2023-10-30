3fad2a3feb1b70190a7eca59104982ececd17e96
Alyona <alena210701@mail.ru>
2023-10-30 23:25:24 +0300

    lab3_task1-2

--- labOS/lab3/2.c
+++ labOS/lab3/2.c
@@ -1,30 +1,39 @@
 //
-//  1.c
+//  2.c
 //  labOS
 //
 //  Created by Alyona Borushnova on 30.10.2023.
 //
 
-//Изменить реакцию процесса на некоторый сигнал при помощи signal. В функции обработчике переустановить реакцию на сигнал на значение по умолчанию. Запустить программу и объяснить наблюдаемый результат.
+// Модифицировать программу п.1 так, чтобы вместо signal использовать sigaction.
 
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 
-/** Пользовательская функция-обработчик SIGUSR1 */
+struct sigaction new_action, old_action;
+
 void signal_handler(int signum) {
     
     printf("Received signal: %d\n", signum);
     
     // Восстанавливаем действие по умолчанию
-    signal(signum, SIG_DFL);
+    sigaction(signum, &old_action, NULL);
 }
 
 int main(int argc, char *argv[]) {
     
     printf("PID: %d\n", getpid());
     
-    signal(SIGUSR1, signal_handler);
+    new_action.sa_handler = signal_handler;
+    new_action.sa_flags = 0;
+    sigemptyset(&new_action.sa_mask);
+
+    // Установка нового действия с использованием sigaction
+    if (sigaction(SIGUSR1, &new_action, &old_action) == -1) {
+        perror("sigaction");
+        return 1;
+    }
 
     printf("Sending SIGUSR1...\n");
     // Отправляем  SIGUSR1
@@ -34,6 +43,6 @@ int main(int argc, char *argv[]) {
         // Отправляем  SIGUSR1, чтобы увидеть реакцию на сигнал по умолчанию (завершился...)
         // kill -USR1 <PID>
     }
-    
+
     return 0;
 }
